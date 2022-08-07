2fe7b1d72932956d40e2f9cad48ee3d23b2c8ab6
Markus Wohlan <markuswohlan@gmail.com>
2022-08-07 12:16:31 +0200

    current state

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
   ssr: true,
   experimental: {
     reactivityTransform: false,
-    viteNode: false,
+    viteNode: true,
   },
   unocss: {
     preflight: false,
@@ -38,11 +38,11 @@ export default defineNuxtConfig({
     '@unocss/reset/tailwind.css',
     'floating-vue/dist/style.css',
   ],
-  // vite: {
-  //   optimizeDeps: {
-  //     include: ['pinia'],
-  //   },
-  // },
+  vite: {
+    optimizeDeps: {
+      include: ['pinia'],
+    },
+  },
   hooks: {
     'vite:extendConfig': function (config: any, { isServer }: any) {
       if (isServer) {
